3c383e1d8dba767631586f9ce2a2612dae5bc772
Marwen Haouacine <marwen75@gmail.com>
2020-10-24 16:20:09 +0200

    App is now responsive

---
+++ LeBaluchon/View/RoundButton.swift
@@ -0,0 +1,31 @@
+//
+//  RoundButton.swift
+//  LeBaluchon
+//
+//  Created by Marwen Haouacine on 24/10/2020.
+//  Copyright © 2020 marwen. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+
+       @IBInspectable var cornerRadius: CGFloat = 0{
+           didSet{
+           self.layer.cornerRadius = cornerRadius
+           }
+       }
+
+       @IBInspectable var borderWidth: CGFloat = 0{
+           didSet{
+               self.layer.borderWidth = borderWidth
+           }
+       }
+
+       @IBInspectable var borderColor: UIColor = UIColor.clear{
+           didSet{
+               self.layer.borderColor = borderColor.cgColor
+           }
+       }
+}
